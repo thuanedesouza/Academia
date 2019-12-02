c735e5ea56453fd89c7c88bc705ba012c188037e
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2019-12-01 22:50:08 -0300

    imagens

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <style type="text/css">
     #inputbtn:hover{cursor:pointer;}
   </style>
-  <body style="background:url('images/2.jpg'); background-size: cover;">
+  <body style="background:url('images/0.jpg'); background-size: cover;">
     <div class="container-fluid" style="margin-top:60px;margin-bottom:60px;color:#34495E;">
       <div class="row">
         <div class="col-md-1"></div>
